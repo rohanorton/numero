80e4f105014a1f9605209f23013f1fd23040e823
Rohan Orton <rohan.orton@gmail.com>
2017-10-03 14:34:20 +0100

    change to object and prettier

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const numero = (n) => {
-    const digits = [ 'zero', 'uno', 'due', 'tre', 'quattro', 'cinque', 'sei', 'sette', 'otto', 'nove' ]
-    return digits[n]
-}
+const numero = n => {
+  const digitMap = {
+    '0': 'zero',
+    '1': 'uno',
+    '2': 'due',
+    '3': 'tre',
+    '4': 'quattro',
+    '5': 'cinque',
+    '6': 'sei',
+    '7': 'sette',
+    '8': 'otto',
+    '9': 'nove',
+  };
+  return digitMap[n];
+};
 
-module.exports = numero
+module.exports = numero;
